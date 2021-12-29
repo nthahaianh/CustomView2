b17d65cfb36c6beeb5c6e9883c1aeee46f463c2c
nthahaianh <ntha.haianh@gmail.com>
2021-12-29 10:10:12 +0700

    update auto move

--- app/src/main/java/com/example/customview2/customview/BallsView.kt
+++ app/src/main/java/com/example/customview2/customview/BallsView.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import com.example.customview2.customview.MyBall
 import com.example.customview2.R
 import kotlinx.coroutines.*
@@ -20,23 +21,22 @@ class BallsView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     companion object {
-        private const val DEFAULT_RADIUS = 30F // default
-        const val TIME_TO_RETURN = 200L
-        const val TIME_DELAY_AUTO = 300L
-        const val DELTA_AUTO = 10
-        const val SUM_BALLS_DEFAULT = 5
-        val listColor = mutableListOf(
+        private const val DEFAULT_RADIUS = 30F
+        private const val TIME_TO_RETURN = 200L
+        private const val TIME_DELAY_AUTO = 300L
+        private const val DELTA_AUTO = 10
+        private const val SUM_BALLS_DEFAULT = 5
+        private val listColor = mutableListOf(
             Color.GREEN,
             Color.RED,
             Color.BLUE,
             Color.YELLOW
         )
     }
-
     private var listBalls: MutableList<MyBall> = mutableListOf()
     private lateinit var ballPaint: Paint
     private var radius = DEFAULT_RADIUS
-    var sumBall = 0
+    var sumBall = SUM_BALLS_DEFAULT
     private var initCoordinatesX = 0
     private var initCoordinatesY = 0
     private var isAuto = true
@@ -57,7 +57,6 @@ class BallsView @JvmOverloads constructor(
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        Log.e("onMeasure", "-------------------------------")
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val widthSpecSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightSpecSize = MeasureSpec.getSize(heightMeasureSpec)
@@ -92,12 +91,7 @@ class BallsView @JvmOverloads constructor(
                 isAntiAlias = true
                 style = Paint.Style.FILL
             }
-            canvas?.drawCircle(
-                listBalls[i].x,
-                listBalls[i].y,
-                radius,
-                ballPaint
-            )
+            canvas?.drawCircle(listBalls[i].x, listBalls[i].y, radius, ballPaint)
         }
     }
 
@@ -114,23 +108,17 @@ class BallsView @JvmOverloads constructor(
 
             MotionEvent.ACTION_MOVE -> {
                 for (i in listBalls.size - 1 downTo 1) {
-                    if (abs(listBalls[i].x - listBalls[i - 1].x) > 5) {
-                        listBalls[i].x = listBalls[i - 1].x
-                    }
-                    if (abs(listBalls[i].y - listBalls[i - 1].y) > 5) {
-                        listBalls[i].y = listBalls[i - 1].y
-                    }
+                    listBalls[i].x = listBalls[i - 1].x
+                    listBalls[i].y = listBalls[i - 1].y
                 }
                 listBalls[0].x = event.x
                 listBalls[0].y = event.y
-                invalidate()
             }
 
             MotionEvent.ACTION_UP -> {
                 isBack = true
                 for (i in 0 until listBalls.size) {
-                    val x =
-                        initCoordinatesX - (2 * i + 1) * (initCoordinatesX / (2 * sumBall)).toFloat()
+                    val x = initCoordinatesX - (2 * i + 1) * (initCoordinatesX / (2 * sumBall)).toFloat()
                     val y = initCoordinatesY / 2.toFloat()
                     ballMoveBack(i, x, y)
                 }
@@ -140,26 +128,54 @@ class BallsView @JvmOverloads constructor(
         return true
     }
 
-    private var jobAuto: Job? = null
     private fun autoUpAndDown() {
-        for (i in 0 until listBalls.size) {
-            val ball = listBalls[i]
-            jobAuto = CoroutineScope(Dispatchers.Default).launch {
-                delay(TIME_DELAY_AUTO * i)
-                if (isAuto && isActive) {
-                    if (ball.isUp) {
-                        ball.y -= DELTA_AUTO
-                    } else {
-                        ball.y += DELTA_AUTO
-                    }
-                    if (abs(ball.y - initCoordinatesY / 3.toFloat()) <= DELTA_AUTO)
-                        ball.isUp = false
-                    if (abs(ball.y - 2 * initCoordinatesY / 3) <= DELTA_AUTO)
-                        ball.isUp = true
-                }
+        val ball = listBalls[0]
+        if (isAuto) {
+            if (ball.isUp) {
+                ball.y -= DELTA_AUTO
+            } else {
+                ball.y += DELTA_AUTO
+            }
+            if (abs(ball.y - initCoordinatesY / 3.toFloat()) <= DELTA_AUTO)
+                ball.isUp = false
+            if (abs(ball.y - 2 * initCoordinatesY / 3) <= DELTA_AUTO)
+                ball.isUp = true
+        }
+        for (i in 1 until listBalls.size) {
+            val y = listBalls[i - 1].y
+            moveBall(i, y)
+        }
+    }
+
+    private var jobAuto: Job? = null
+    private fun moveBall(i: Int, newY: Float) {
+        val ball = listBalls[i]
+        jobAuto = CoroutineScope(Dispatchers.Default).launch {
+            delay(TIME_DELAY_AUTO)
+            if (isAuto && isActive) {
+                ball.y = newY
             }
         }
     }
+//    private fun autoUpAndDown() {
+//        for (i in 0 until listBalls.size) {
+//            val ball = listBalls[i]
+//            jobAuto = CoroutineScope(Dispatchers.Default).launch {
+//                delay(TIME_DELAY_AUTO * i)
+//                if (isAuto && isActive) {
+//                    if (ball.isUp) {
+//                        ball.y -= DELTA_AUTO
+//                    } else {
+//                        ball.y += DELTA_AUTO
+//                    }
+//                    if (abs(ball.y - initCoordinatesY / 3.toFloat()) <= DELTA_AUTO)
+//                        ball.isUp = false
+//                    if (abs(ball.y - 2 * initCoordinatesY / 3) <= DELTA_AUTO)
+//                        ball.isUp = true
+//                }
+//            }
+//        }
+//    }
 
     private var jobBack: Job? = null
     private fun ballMoveBack(i: Int, newX: Float, newY: Float) {
@@ -188,8 +204,7 @@ class BallsView @JvmOverloads constructor(
 
     inner class DelayThread : Runnable {
         override fun run() {
-            val x =
-                initCoordinatesX - (2 * (sumBall - 1) + 1) * (initCoordinatesX / (2 * sumBall)).toFloat()
+            val x = initCoordinatesX - (2 * (sumBall - 1) + 1) * (initCoordinatesX / (2 * sumBall)).toFloat()
             val y = initCoordinatesY / 2.toFloat()
             if (listBalls.last().x == x && listBalls.last().y == y) {
                 for (ball in listBalls) {
